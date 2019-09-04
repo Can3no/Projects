1ddea82043cf5e8d1e2b7d3c61aebf8d5c2dc318
Your Name <you@example.com>
2019-09-04 20:47:21 +0300

    muutoksia

--- Osa1/kurssitiedot/src/index.js
+++ Osa1/kurssitiedot/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
+const App = (props) => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
@@ -10,37 +10,44 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
-  const Content = ... {
+  const Header = () =>  {
+      return (
+        <div>
+        <h1>{props.course}</h1>
+        </div>
+      )
+    }
+
+    const Part
+
+    const Content = () =>  {
+      return (
+        <div>
+        <p>
+          {part1} {exercises1}
+        </p>
+        <p>
+          {part2} {exercises2}
+        </p>
+        <p>
+          {part3} {exercises3}
+        </p>
+        </div>
+      )
+    }
+
+    const Total = () =>  {
+      return (
+        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      )
+    }
+
     return (
       <div>
-        <Part part1/>
-        <Part part2/>
-        <Part part3/>
+      <Header  />
+      <Content  />
+      <Total  />
       </div>
     )
-  }
-
-  return (
-    <div>
-    <Header course={course} />
-    <Content  />
-    <Total />
-    </div>
-  
-    <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
-  )
 }
-
 ReactDOM.render(<App />, document.getElementById('root'))
